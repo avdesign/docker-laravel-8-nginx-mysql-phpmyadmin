8795b851c769f9b2c334badce6b4d4587dc04ec4
Anselmo Velame <suporte.avdesign@gmail.com>
2020-10-03 09:37:17 -0300

    README

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Place;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+//use Illuminate\Support\Facades\DB;
 
 class PlaceController extends Controller
 {
+    private $model;
+
+
+    public function __construct(Place $model)
+    {
+        $this->model = $model;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,8 +23,13 @@ class PlaceController extends Controller
      */
     public function index()
     {
+        $visited = $this->model->whereVisited(1)->get();
+        $togo = $this->model->whereVisited(0)->get();
+
+        /*
         $visited = DB::select('select * from places where visited = ?', [1]); 
         $togo = DB::select('select * from places where visited = ?', [0]);
+        */
 
         return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
     }
